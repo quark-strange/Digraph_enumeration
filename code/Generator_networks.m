270d6be1137d8b816ab7d4099d0017a94b65ff16
quark-strange <55325650+quark-strange@users.noreply.github.com>
2020-06-25 15:50:43 -0400

    Update Generator_networks.m

--- code/Generator_networks.m
+++ code/Generator_networks.m
@@ -5,26 +5,12 @@ matrix_vec = [];
 
 for i = 1000001 : s
     v = x(i,:);
+    
+    % check whether v is strongly connected 
     G = digraph(adjacent_matrix(v));
     bins = conncomp(G, 'Type', 'strong');
     if all(bins == 1)
         matrix_vec = [matrix_vec; v];
     end
-    if mod(i,10000) == 0
-        i
-    end
+    
 end
-
-function adj_matr = adjacent_matrix(v)
-
-adj_matr = zeros(5,5);
-adj_matr(2:5,1) = v(1:4);
-adj_matr(1,2) = v(5);
-adj_matr(3:5,2) = v(6:8);
-adj_matr(1:2,3) = v(9:10);
-adj_matr(4:5,3) = v(11:12);
-adj_matr(1:3,4) = v(13:15);
-adj_matr(5,4) = v(16);
-adj_matr(1:4,5) = v(17:20);
-end
-
