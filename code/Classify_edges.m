922ae1dc062e72b94fd99588e495c201300559d4
quark-strange <55325650+quark-strange@users.noreply.github.com>
2020-06-25 15:51:07 -0400

    Update Classify_edges.m

--- code/Classify_edges.m
+++ code/Classify_edges.m
@@ -1,14 +1,4 @@
 s = 2^20;
 load('matrix_vec2.mat');
 
-for i = 5 : 20
-    class = ['digraph_',mat2str(i)];
-    eval([class, '= [];']);
-end
 
-for i = 1 : size(matrix_vec2,1)
-    v = matrix_vec2(i,:);
-    edge_num = sum(v);
-    class = ['digraph_',mat2str(edge_num)];
-    eval([class, '= [eval(class); v];']); 
-end
