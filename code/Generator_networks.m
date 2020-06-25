3c5569022129da68b6c2600f06143edf30badd3e
quark-strange <55325650+quark-strange@users.noreply.github.com>
2020-06-25 15:53:22 -0400

    Update Generator_networks.m

--- code/Generator_networks.m
+++ code/Generator_networks.m
@@ -1,9 +1,16 @@
-s = 2^20;
+% number of vertices
+n = 5;
+
+% maximum number of edges
+s = 2^(n * n - n);
+
+% generating all possible digraphs
 x = de2bi(0:2^20-1);
 
+% generating all strongly connected graph
 matrix_vec = [];
 
-for i = 1000001 : s
+for i = 1 : s
     v = x(i,:);
     
     % check whether v is strongly connected 
@@ -13,4 +20,17 @@ for i = 1000001 : s
         matrix_vec = [matrix_vec; v];
     end
     
+end
+
+% classify according the number of edges
+for i = 5 : 20
+    class = ['digraph_',mat2str(i)];
+    eval([class, '= [];']);
+end
+
+for i = 1 : size(matrix_vec,1)
+    v = matrix_vec(i,:);
+    edge_num = sum(v);
+    class = ['digraph_',mat2str(edge_num)];
+    eval([class, '= [eval(class); v];']); 
 end
